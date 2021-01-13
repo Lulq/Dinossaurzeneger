fe90930080d88a0a5f60c88a36571f3e3aa551db
Luiz Antonio Negrinho <luizavm@gmail.com>
2021-01-13 20:02:24 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,89 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
+let position = 0;
+var fxjump = new Audio('jump.wav');
+var fxdeath = new Audio('death.wav');
+let dead = false;
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        if(!isJumping){
+        jump();
+        fxjump.play();
+        
+        } 
+    }
+}
+
+function handleKeyDown(event) {
+    if (event.keyCode === 32) {
+        if(dead){
+            window.location.reload();
+            
+        }
+    }
+
+}
+
+function jump() {
+    isJumping = true;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+            //descendo
+            let downInterval = setInterval(() => {
+                if (position <= 0){
+                    clearInterval(downInterval);
+                    isJumping = false;
+                }else{
+                position -= 20;
+                dino.style.bottom = position +'px';
+                }
+            }, 20);
+
+        }else {
+        //subindo
+        position += 20;
+        dino.style.bottom = position + 'px';
+        }
+    }, 20);
+}
+
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        cactusPosition -= 10;
+        cactus.style.left = cactusPosition +'px';
+        if (cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            //quando o cacto bate no dinossauro
+            clearInterval(leftInterval);
+            document.body.innerHTML= '<h1 class="game-over">Fim de Jogo</h1></br><h3 class="continue">Tente outra vez</h3>';
+            dead = true;
+            fxdeath.play();
+            
+                                
+        }else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition +'px';
+        }
+    }, 20);
+    if (!dead) {
+        setTimeout(createCactus, randomTime)
+    }
+}
+ 
+createCactus();
+document.addEventListener('keyup', handleKeyUp); //espera ação da tecla pela função criada no começo do jogo
+document.addEventListener('keydown', handleKeyDown); //reinicia o jogo
